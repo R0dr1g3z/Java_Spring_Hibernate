fa166383264369b57483180e3d87bfab17da5748
R0dr1g3z <r0dr1g3zek@gmail.com>
2023-02-04 17:09:52 +0100

    add task1 JPQL

--- src/main/java/com/example/spring_hibernate/Controller/BookController.java
+++ src/main/java/com/example/spring_hibernate/Controller/BookController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.spring_hibernate.Class.Author;
@@ -54,5 +55,11 @@ public class BookController {
         int id = 1;
         bookDao.deleteById(id);
         return String.format("Book by ID:%s is deleted", id);
+    }
+
+    @GetMapping("/readBooksByRating/{rating}")
+    @ResponseBody
+    public List<Book> readBooksByRating(@PathVariable int rating){
+        return bookDao.findAllBooksByRating(rating);
     }
 }
--- src/main/java/com/example/spring_hibernate/Service/BookDao.java
+++ src/main/java/com/example/spring_hibernate/Service/BookDao.java
@@ -1,5 +1,8 @@
 package com.example.spring_hibernate.Service;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.spring_hibernate.Class.Book;
@@ -7,4 +10,7 @@ import com.example.spring_hibernate.Class.Book;
 public interface BookDao extends CrudRepository<Book, Integer> {
     
     Book findById(int id);
+
+    @Query(value = "SELECT b FROM Book b WHERE b.rating > ?1")
+    List<Book> findAllBooksByRating(int rating);
 }
