bd28943a2c3c8ea75e50c2f2f95caf414ff9d20e
R0dr1g3z <r0dr1g3zek@gmail.com>
2023-02-05 21:42:04 +0100

    add task3 spring validate

--- src/main/java/com/example/spring_hibernate/Class/Author.java
+++ src/main/java/com/example/spring_hibernate/Class/Author.java
@@ -3,12 +3,16 @@ package com.example.spring_hibernate.Class;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.validator.constraints.pl.PESEL;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,8 +27,14 @@ public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @NotEmpty
     private String firstName;
+    @NotEmpty
     private String lastName;
+    @PESEL
+    private int pesel;
+    @Email
+    private String email;
     @ManyToMany
     private List<Book> books = new ArrayList<>();
 }
--- src/main/java/com/example/spring_hibernate/Class/Publisher.java
+++ src/main/java/com/example/spring_hibernate/Class/Publisher.java
@@ -3,12 +3,16 @@ package com.example.spring_hibernate.Class;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.validator.constraints.pl.NIP;
+import org.hibernate.validator.constraints.pl.REGON;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,7 +27,12 @@ public class Publisher {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @NotEmpty
     private String name;
+    @NIP
+    private int nip;
+    @REGON
+    private int regon;
     @OneToMany
     private List<Book> books = new ArrayList<>();
 }
